e07ba453b4a57a6d7ff8d46265238358b3364ab4
Manu-Madhu <manum66466@gmail.com>
2026-02-22 12:52:21 +0530

    server updated with some remove thigs

--- src/config/db.js
+++ src/config/db.js
@@ -18,8 +18,6 @@ const connectDB = async () => {
             socketTimeoutMS: 45000,
             maxPoolSize: 10, // Maintain up to 10 socket connections
             minPoolSize: 2, // Maintain at least 2 socket connections
-            bufferCommands: false, // Disable mongoose buffering
-            bufferMaxEntries: 0, // Disable mongoose buffering
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
